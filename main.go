4e74089271681ce2540e25a26b4a2d4d9eecf7b6
nvm <nevermn@protonmail.com>
2020-02-26 15:15:36 +0200

    Fix usage message

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ func init() {
 }
 
 func printUsage() {
-	fmt.Println("Usage: ttt -h 127.0.0.1:9001 to connect to a server")
-	fmt.Println("ttt -p 9001 to start a server listening on 9001 port")
+	fmt.Println("Usage: tictactoe -c 127.0.0.1:9001 to connect to a server")
+	fmt.Println("tictactoe -p 9001 to start a server listening on 9001 port")
 }
 
 func main() {
